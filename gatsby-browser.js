a4ae46571d3b062aa09811f5fb8453813f4beb6a
hassan <hmotawe@effvision.com>
2020-07-13 20:03:51 +0200

    rearrange styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+import 'bootstrap/dist/css/bootstrap.css'
+import "slick-carousel/slick/slick-theme.css";
+
+import "slick-carousel/slick/slick.css";
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
