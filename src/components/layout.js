96fcdb7ca5c95ae436d8876eb74983fafa7b8ad9
hassan <hmotawe@effvision.com>
2020-07-13 19:28:54 +0200

    review and scroll fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React ,{ useEffect }  from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql, withPrefix } from "gatsby"
 import Helmet from 'react-helmet'
@@ -14,7 +14,7 @@ import Footer from './footer'
 import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 import "../assets/scss/typography/typography.scss"
 import '../assets/scss/components/layout.scss'
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Layout = ({ children }) => {
@@ -28,18 +28,14 @@ const Layout = ({ children }) => {
     }
   `)
   // Similar to componentDidMount and componentDidUpdate: 
-  
- 
+
+
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>
-    
-  
-
         <Footer />
-         
       </div>
     </>
   )
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
 import Img from 'gatsby-image'
-
+import review from '../assets/scss/components/review.module.scss'
 const Reviews = ({ node }) => {
     const data = useStaticQuery(graphql`
     query {
-        review: file(relativePath: { eq: "product-images/review-mob.png" }) {
+        review: file(relativePath: { eq: "product-images/review.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
           }
         }
       }
-  
+      reviewmob: file(relativePath: { eq: "product-images/review-mob.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }`
     )
-    return (<div> <Img fluid={data.review.childImageSharp.fluid} /></div>)
+    return (<div className={"container-fluid"}>
+        <div className={"row"}>
+            <div className={["col-12", "col-lg-10", "offset-lg-1"].join(" ")}>
+                <Img className={review.deskreview} fluid={data.review.childImageSharp.fluid} />
+                <Img className={review.mobreview} fluid={data.reviewmob.childImageSharp.fluid} />
+            </div>
+        </div>
+    </div>
+    )
 }
-    export default Reviews;
+export default Reviews;
