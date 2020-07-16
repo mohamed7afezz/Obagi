eb82419b4a4775821d2f13821fddb7cd7855ceea
Ahmed Bahieddine <abahi@effvision.com>
2020-07-16 15:33:59 -0400

    produt hero dynamic

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -3,38 +3,61 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 import { getParagraph } from "../components/paragraphs-helper";
+import ProductHero from '../components/product-components/product-hero';
 
-const ProductPage = () => {
+const ProductPage = ({data}) => {
 
     // const paragraphs = data.nodePage.relationships.paragraphs.map(getParagraph);
 
     return (
         <Layout>
+            <ProductHero node={data.nodeClinicalProduct} />
             {/* {paragraphs} */}
-            <h1>Detailed Product page</h1>
         </Layout>
     )
 }
 
 export default ProductPage;
 
-// export const productPageQuery = graphql`
-//     query($slug: String!) {
-//         nodeProducts(fields: { slug: { eq: $slug } }) {
+export const productPageQuery = graphql`
+    query($slug: String!) {
+        nodeClinicalProduct(fields: { slug: { eq: $slug } }) {
+            title
+            field_clinical_price
+            field_clinical_upc
+            field_clinical_id
+            field_clinical_medical_type
+            field_clinical_weight
+            field_clinical_description {
+                processed
+            }
+            relationships {
+                field_clinical_skin_concern {
+                    name
+                    path {
+                        alias
+                    }
+                }
+                field_clinical_image {
+                    localFile {
+                        childImageSharp {
+                            fluid {
+                                ...GatsbyImageSharpFluid
+                            }
+                            fixed(width: 587) {
+                                ...GatsbyImageSharpFixed
+                            }
+                        }
+                    }
+                }
+                field_clinical_skin_type {
+                    name
+                    path {
+                        alias
+                    }
+                }
+            }
             
-//             relationships {
-//                 paragraphs: field_paragraphs {
-//                     type: __typename
-//                     ...paragraphHomeHero
-//                     ...paragraphFullHtmlContent
-//                     ...paragraphSectionWithColoredBoxes
-//                     ...paragraphSectionWithImageAndText
-//                     ...paragraphImageLeftTextRight
-//                     ...paragraphBigParagraph
-//                     ...paragraphImagesBoxes
-//                 }
-//             }
-            
-//         }
-//     }
-// `;
+        }
+    }
+`;
