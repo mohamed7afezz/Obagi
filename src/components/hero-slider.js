d8c6f6ca4e157ef2bdd8597894b0db1ddd4b0122
Ashraqat <ashraqatelshaabany@gmail.com>
2020-07-24 18:53:55 +0200

    incomplete edits on hero slider

--- src/components/hero-slider.js
+++ src/components/hero-slider.js
@@ -1,16 +1,45 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
+import Slider from "react-slick"
 
 const HeroSlider = ({ node }) => {
 
+    const SliderSetting = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        arrows: false,
+        dots : true,
+        beforeChange: (current, next) => {
+          document.querySelector('.myslickactive').classList.remove('myslickactive'); 
+          document.querySelectorAll("#product-hero-slick .imageContainer")[next].classList.add('myslickactive')
+        },
+        responsive: [
+          {          
+            breakpoint: 1024,
+            settings: {
+              dots: false,
+            }
+          },
+      ]
+      }
+
+
     return (
         <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <div><Img fluid={node.relationships.field_slider_image.localFile.childImageSharp.fluid}/></div>
+                    </div>
+                </div>
+            </div>
             <div dangerouslySetInnerHTML={{__html: node.field_type.processed}}></div>
             <div dangerouslySetInnerHTML={{__html: node.field_slider_title.processed}}></div>
             <div dangerouslySetInnerHTML={{__html: node.field_slider_subtitle.processed}}></div>
             <div><Link to={node.field_slider_button.uri}>{node.field_slider_button.title}</Link></div>
-            <div><Img fluid={node.relationships.field_slider_image.localFile.childImageSharp.fluid}/></div>
+
             <div><Link to="#"><Img fixed={node.relationships.field_slider_scroll_down.localFile.childImageSharp.fixed}/></Link></div>
         </div>
     )
