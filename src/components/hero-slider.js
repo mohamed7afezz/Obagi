79edfb9ed482736a0f40904283608556debe7586
Ashraqat <ashraqatelshaabany@gmail.com>
2020-07-26 20:18:11 +0200

    hero slider

--- src/components/hero-slider.js
+++ src/components/hero-slider.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 import Slider from "react-slick"
+import heroSlider from '../assets/scss/components/hero-slider.module.scss'
 
 const HeroSlider = ({ node }) => {
 
@@ -10,37 +11,93 @@ const HeroSlider = ({ node }) => {
         speed: 500,
         slidesToShow: 1,
         arrows: false,
-        dots : true,
-        beforeChange: (current, next) => {
-          document.querySelector('.myslickactive').classList.remove('myslickactive'); 
-          document.querySelectorAll("#product-hero-slick .imageContainer")[next].classList.add('myslickactive')
-        },
+        dots: true,
+        // beforeChange: (current, next) => {
+        //     document.querySelector('.myslickactive').classList.remove('myslickactive');
+        //     document.querySelectorAll("#product-hero-slick .imageContainer")[next].classList.add('myslickactive')
+        // },
         responsive: [
-          {          
-            breakpoint: 1024,
-            settings: {
-              dots: false,
-            }
-          },
-      ]
-      }
+            {
+                breakpoint: 1024,
+                settings: {
+                    dots: false,
+                }
+            },
+        ]
+    }
 
 
     return (
-        <div>
+        <div className="hero-slider">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col">
-                        <div><Img fluid={node.relationships.field_slider_image.localFile.childImageSharp.fluid}/></div>
+                    <div style={{ width: "100%" }}>
+                        <Slider {...SliderSetting}>
+                            <div>
+                                <div className="d-lg-none">
+                                    <div className="col-12 col-lg-7 col-padding">
+                                        <div><Img fluid={node.relationships.field_slider_image.localFile.childImageSharp.fluid} /></div>
+                                    </div>
+                                    <div className={heroSlider.textWrapper}>
+                                        <div className="col-12 col-lg-4">
+                                            <div dangerouslySetInnerHTML={{ __html: node.field_type.processed }} className={["subtitle", heroSlider.subtitle].join(" ")}></div>
+                                            <div dangerouslySetInnerHTML={{ __html: node.field_slider_title.processed }} className={[heroSlider.title].join(" ")}></div>
+                                            <div dangerouslySetInnerHTML={{ __html: node.field_slider_subtitle.processed }} className={[heroSlider.description].join(" ")}></div>
+                                            <div className={heroSlider.linkSection}><Link to={node.field_slider_button.uri} className="button-link">{node.field_slider_button.title}</Link></div>
+                                            {/* <div className="d-none d-lg-block"><Link to="#"><Img fixed={node.relationships.field_slider_scroll_down.localFile.childImageSharp.fixed} /></Link></div> */}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="d-none d-lg-block">
+                                    <div className="row">
+
+                                    {/* <div className={heroSlider.textWrapper}> */}
+                                            <div className={["col-lg-5", heroSlider.textWrapper].join(" ")}>
+                                                <div className="col-11 offset-1 col-right-padding">
+                                                <div dangerouslySetInnerHTML={{ __html: node.field_type.processed }} className={["subtitle", heroSlider.subtitle].join(" ")}></div>
+                                                <div dangerouslySetInnerHTML={{ __html: node.field_slider_title.processed }} className={[heroSlider.title].join(" ")}></div>
+                                                <div dangerouslySetInnerHTML={{ __html: node.field_slider_subtitle.processed }} className={[heroSlider.description].join(" ")}></div>
+                                                <div className={heroSlider.linkSection}><Link to={node.field_slider_button.uri} className="button-link">{node.field_slider_button.title}</Link></div>
+                                                {/* <div className="d-none d-lg-block"><Link to="#"><Img fixed={node.relationships.field_slider_scroll_down.localFile.childImageSharp.fixed} /></Link></div> */}
+                                                </div>
+                                            </div>
+                                        {/* </div> */}
+                                        <div className="col-md-7 col-padding">
+                                            <div><Img fluid={node.relationships.field_slider_image.localFile.childImageSharp.fluid} /></div>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+
+                            <div>
+                                <div className="row">
+                                    <div className="col col-padding">
+                                        <div><img src={node.relationships.field_slider_image.localFile.childImageSharp.original.src} /></div>
+                                    </div>
+                                    <div className="col">
+                                        <div dangerouslySetInnerHTML={{ __html: node.field_type.processed }}></div>
+                                        <div dangerouslySetInnerHTML={{ __html: node.field_slider_title.processed }}></div>
+                                        <div dangerouslySetInnerHTML={{ __html: node.field_slider_subtitle.processed }}></div>
+                                        <div><Link to={node.field_slider_button.uri}>{node.field_slider_button.title}</Link></div>
+                                        <div className="d-none d-lg-block"><Link to="#"><Img fixed={node.relationships.field_slider_scroll_down.localFile.childImageSharp.fixed} /></Link></div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </Slider>
                     </div>
+
+
+
                 </div>
             </div>
-            <div dangerouslySetInnerHTML={{__html: node.field_type.processed}}></div>
-            <div dangerouslySetInnerHTML={{__html: node.field_slider_title.processed}}></div>
-            <div dangerouslySetInnerHTML={{__html: node.field_slider_subtitle.processed}}></div>
-            <div><Link to={node.field_slider_button.uri}>{node.field_slider_button.title}</Link></div>
-
-            <div><Link to="#"><Img fixed={node.relationships.field_slider_scroll_down.localFile.childImageSharp.fixed}/></Link></div>
         </div>
     )
 }
@@ -68,6 +125,9 @@ export const fragment = graphql`
                         childImageSharp {
                             fluid {
                                 ...GatsbyImageSharpFluid
+                            }
+                            original {
+                                src
                             }
                         }
                     }
