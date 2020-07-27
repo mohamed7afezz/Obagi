a186b3f296b62f438d651e7d03d2dd3c90d982cd
Ashraqat <ashraqatelshaabany@gmail.com>
2020-07-27 15:48:01 +0200

    featured section edits

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,24 +1,89 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
 import featuredStyles from '../assets/scss/components/featured.module.scss'
+import Img from 'gatsby-image'
+import Player from '@vimeo/player'
+import playbtnimg from "../assets/images/product-images/PlayVideo.svg"
 
+function playvideo(event) {
+  let iframeContainer, player, playerOpts = {
+    url: ''
+  }
+  console.log("hassan",)
+  let url = event.target.parentNode.getAttribute("href")
+  console.log(url)
+  playerOpts.url = url
+  console.log(playerOpts.url)
+  if (!playerOpts.url.indexOf('youtube') > -1) {
+    document.querySelector('.video-popup-wrap').innerHTML = '<iframe class="embed-responsive-item" src="' + url + '?rel=0&autoplay=true" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    console.log(document.querySelector('.video-popup-wrap'))
+    return;
+  }
+
+  player = new Player.Vimeo(document.querySelector('#VideoPopUp iframe'), playerOpts);
+
+  player.play();
+
+}
 
 const Featured = ({ node }) => {
 
   return (
-    <div className={["container-fluid", featuredStyles.wrapper].join(" ")}>
-      <div className="row">
-        {/* <div className="col-12">
-            </div> */}
-        <div className="col-12">
-          <div className={featuredStyles.textWrapper}>
-            <div className={["subtitle", featuredStyles.subtitle].join(" ")}>Featured</div>
-            <div dangerouslySetInnerHTML={{ __html: node.field_featured_title.processed }} className={featuredStyles.title}></div>
-            <div className={featuredStyles.products}>PRODUCTS (<span className={featuredStyles.productsNo}>18</span>)</div>
-            <div dangerouslySetInnerHTML={{ __html: node.field_featured_description.processed }} className={featuredStyles.description}></div>
-            <div className={featuredStyles.perfect}>PERFECT FOR: <span>{node.relationships.field_issues_categories.taxonomy_term__clinical_categories.name}</span></div>
-            <div className={featuredStyles.linkSection}><Link to={node.field_featured_button.uri} className="button-link">{node.field_featured_button.title}</Link></div>
-            {/* <div></div> */}
+    <div>
+      <div className={["container-fluid", "d-lg-none", featuredStyles.wrapper].join(" ")}>
+        <div className="row">
+          <div className="col-12">
+
+            <div className="video-wrapper">
+
+              <div className="img-wrap">
+                <a class="popupvideo" data-toggle="modal" data-target="#VideoPopUp" onClick={(e) => { playvideo(e) }} href={node.relationships.field_feautured_video ? node.relationships.field_featured_video.field_video_link : ''} class="playbtn">
+                  <img class="playbtnimg" src={playbtnimg} alt="videomsg" />
+                </a>
+                {node.relationships.field_featured_video ? <Img fluid={node.relationships.field_featured_video.relationships.field_video_poster.localFile.childImageSharp.fluid} /> : ''}
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className={featuredStyles.textWrapper}>
+              <div className={["subtitle", featuredStyles.subtitle].join(" ")}>Featured</div>
+              <div dangerouslySetInnerHTML={{ __html: node.field_featured_title.processed }} className={featuredStyles.title}></div>
+              <div className={featuredStyles.products}>PRODUCTS (<span className={featuredStyles.productsNo}>18</span>)</div>
+              <div dangerouslySetInnerHTML={{ __html: node.field_featured_description.processed }} className={featuredStyles.description}></div>
+              <div className={featuredStyles.perfect}>PERFECT FOR: <span><Link to="#" className={featuredStyles.category}></Link></span></div>
+              <div className={featuredStyles.linkSection}><Link to={node.field_featured_button.uri} className="button-link">{node.field_featured_button.title}</Link></div>
+              {/* <div>{node.relationships.field_issues_categories.taxonomy_term__clinical_categories.name}</div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className={["container-fluid", "d-none", "d-lg-block", featuredStyles.wrapper].join(" ")}>
+        <div className="row">
+          <div className="col-12">
+            <div className={featuredStyles.textWrapper}>
+              <div className={["subtitle", featuredStyles.subtitle].join(" ")}>Featured</div>
+              <div dangerouslySetInnerHTML={{ __html: node.field_featured_title.processed }} className={featuredStyles.title}></div>
+              <div className={featuredStyles.products}>PRODUCTS (<span className={featuredStyles.productsNo}>18</span>)</div>
+              <div dangerouslySetInnerHTML={{ __html: node.field_featured_description.processed }} className={featuredStyles.description}></div>
+              <div className={featuredStyles.perfect}>PERFECT FOR: <span><Link to="#" className={featuredStyles.category}></Link></span></div>
+              <div className={featuredStyles.linkSection}><Link to={node.field_featured_button.uri} className="button-link">{node.field_featured_button.title}</Link></div>
+              {/* <div>{node.relationships.field_issues_categories.taxonomy_term__clinical_categories.name}</div> */}
+            </div>
+          </div>
+
+          <div className="col-12">
+
+            <div className="video-wrapper">
+
+              <div className="img-wrap">
+                <a class="popupvideo" data-toggle="modal" data-target="#VideoPopUp" onClick={(e) => { playvideo(e) }} href={node.relationships.field_feautured_video ? node.relationships.field_featured_video.field_video_link : ''} class="playbtn">
+                  <img class="playbtnimg" src={playbtnimg} alt="videomsg" />
+                </a>
+                {node.relationships.field_featured_video ? <Img fluid={node.relationships.field_featured_video.relationships.field_video_poster.localFile.childImageSharp.fluid} /> : ''}
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -56,6 +121,20 @@ export const fragment = graphql`
               ... on taxonomy_term__medical_categories {
                 id
                 name
+              }
+            }
+            field_featured_video {
+              field_video_link
+              relationships {
+                field_video_poster {
+                  localFile {
+                    childImageSharp {
+                      fluid {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                  }
+                }
               }
             }
           }
