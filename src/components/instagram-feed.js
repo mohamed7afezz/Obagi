d5028324398c8e281e893fe843c4ceb0c67fee17
Ashraqat <ashraqatelshaabany@gmail.com>
2020-07-29 16:58:49 +0200

    instagram feed created

---
+++ src/components/instagram-feed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+const InstagramFeed = ({ node }) => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default InstagramFeed
