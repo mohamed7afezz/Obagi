6fa9fe5e1d66b6eadb07cc01493b8eea6ec052c9
A.Bahi <abahieddine@greaterthanone.com>
2020-08-05 09:54:58 -0400

    fix undefined issue for prod-card

--- src/components/product-components/recommendedparing.js
+++ src/components/product-components/recommendedparing.js
@@ -49,7 +49,7 @@ const Recommendedparing = ({ node }) => {
                node.relationships.field_croduct_card.map((item, index) => (
                  <div className={["col-12", recommendedparing.allcon].join(" ")}>
  
-                   <ProductCard producttitle={item.title} productdescription={{ __html: item.field_medical_description.processed }} productimage={item.relationships.field_medical_image[0].localFile.childImageSharp.fluid} price={item.field_medical_price} rate="5" />
+                   <ProductCard producttitle={item.title} productdescription={{ __html: item.field_medical_description.processed? item.field_medical_description.processed : '' }} productimage={item.relationships.field_medical_image[0].localFile.childImageSharp.fluid} price={item.field_medical_price} rate="5" />
  
                  </div>
                ))
