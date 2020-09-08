66a12dd3a9fd8e1ac69171c06b3f5b42c4c94b94
Ashraqat <ashraqatelshaabany@gmail.com>
2020-09-09 00:40:41 +0200

    review modal finished

--- src/components/review-modal.js
+++ src/components/review-modal.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from 'gatsby-image'
 import reviewStyles from '../assets/scss/components/reviews.module.scss'
-import Stars from './stars'
-import ReviewBox from './review-box'
+import { CustomSelect } from '../assets/js/custom-select'
+
 const ReviewModal = ({ node }) => {
 
+
+    useEffect(() => {
+        if (document.querySelectorAll('.custom-select .select-selected').length < 1) {
+            CustomSelect();
+        }
+    })
+
+    const data = useStaticQuery(graphql`
+    query {
+      productImage: file(relativePath: { eq: "image-3-4.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+    `)
 
 
     return (
@@ -13,16 +31,104 @@ const ReviewModal = ({ node }) => {
             <div className="modal fade review-modal" id="review-modal" tabindex="-1" role="dialog" aria-labelledby="reviewModalLabel" aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
+
+
                         <div className="modal-header">
-                            <h5 className="modal-title" id="reviewModalLabel">Write a Review</h5>
+                            <div className="modal-title" id="reviewModalLabel">Write a Review</div>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close"></button>
                         </div>
+
+
                         <div className="modal-body">
-                           <div className="review-product-name">Vitamin C + Arbutin Brightening Serum</div>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                            <div className="container-fluid">
+                                <div className="row">
+
+                                    {/* Error message if the customer hasn't purchased the item before  */}
+                                    <div className="col-12 col-lg-10 offset-lg-1 d-none">
+                                        <div className="error-wrapper">
+                                            <div className="error-message">Only customers who have purchased this item can review it.</div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12 col-lg-5 offset-lg-1">
+                                        <div className="review-product-name d-lg-none">Vitamin C + Arbutin Brightening Serum</div>
+
+                                        <div className="thankyou-note d-lg-none">Thank you for reviewing your recent purchase!</div>
+                                        <div className="thankyou-footnote d-lg-none">We will send you an email when your review is live.</div>
+
+                                        <div className="review-product-image"><Img fluid={data.productImage.childImageSharp.fluid} /></div>
+                                    </div>
+
+
+                                    <div className="col-12 col-lg-4">
+                                        <div className="review-product-name d-none d-lg-block">Vitamin C + Arbutin Brightening Serum</div>
+
+
+                                        {/* <form>
+                                            <div className="review-footnote">*Required fields</div>
+                                            <div class="form-group select-group">
+                                                <label for="reviewFormSelect" className="form-label">*Rating</label>
+                                                <div className="select-wrapper custom-select">
+                                                    <select class="form-control" id="reviewFormSelect">
+                                                        <option>Select Rating</option>
+                                                        <option>Select Rating</option>
+                                                        <option>5 Star</option>
+                                                        <option>4 Stars</option>
+                                                        <option>3 Star</option>
+                                                        <option>2 Star</option>
+                                                        <option>1 Star</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1" className="form-label">Your Name</label>
+                                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" />
+
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1" className="form-label">*Email</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" />
+
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1" className="form-label">*Review Title</label>
+                                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" />
+
+                                            </div>
+                                            <div class="form-group textarea-group">
+                                                <label for="exampleInputEmail1" className="form-label">*Description</label>
+                                                <textarea type="text" class="form-control textarea-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Type here…" />
+
+                                            </div>
+
+                                            <button type="button" className="captcha">CAPTCHA FPO</button>
+                                            <button type="button" data-dismiss="modal" className="button-link">Submit Review</button>
+                                        </form> */}
+
+
+
+
+                                        {/* After the customer reviews the item  */}
+                                        <div>
+                                            <div className="thankyou-note d-none d-lg-block">Thank you for reviewing your recent purchase!</div>
+                                            <div className="thankyou-footnote d-none d-lg-block">We will send you an email when your review is live.</div>
+                                            <button type="button" className="button-link login-button">Continue Shopping</button>
+                                            <button type="button" className="button-link">Explore More Serums</button>
+                                        </div>
+
+
+                                        {/* If the customer hasn't bought the item:  */}
+                                        <div className="d-none">
+                                            <button type="button" className="button-link login-button">Log In to Review this product</button>
+                                            <button type="button" className="button-link">Continue Shopping</button>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -5,6 +5,7 @@ import reviewStyles from '../assets/scss/components/reviews.module.scss'
 import Stars from './stars'
 import ReviewBox from './review-box'
 import ReviewModal from '../components/review-modal'
+import WelcomeModal from '../components/welcome-modal'
 const Reviews = ({ node }) => {
 
   const data = useStaticQuery(graphql`
@@ -29,16 +30,19 @@ const Reviews = ({ node }) => {
           <div className={reviewStyles.wrapper}>
             <div className={reviewStyles.title}>Product Reviews</div>
             <div className={reviewStyles.stars}><Stars rate="0" /></div>
-            <button type="button" data-toggle="modal" data-target="#review-modal" className={[reviewStyles.reviewButton, "button-link d-lg-none"].join(" ")}>Write a Review <Img fixed={data.pen.childImageSharp.fixed}/></button>
+            <button type="button" data-toggle="modal" data-target="#welcome-modal" className={[reviewStyles.reviewButton, "button-link d-lg-none"].join(" ")}>Write a Review <Img fixed={data.pen.childImageSharp.fixed} /></button>
             <div className={reviewStyles.bottomSection}>
               <div className={reviewStyles.reviewsNumber}>65 Reviews</div>
-              <button type="button" data-toggle="modal" data-target="#review-modal" className={[reviewStyles.reviewButton, "button-link d-none d-lg-flex"].join(" ")}>Write a Review <Img fixed={data.pen.childImageSharp.fixed}/></button>
+              <button type="button" data-toggle="modal" data-target="#welcome-modal" className={[reviewStyles.reviewButton, "button-link d-none d-lg-flex"].join(" ")}>Write a Review <Img fixed={data.pen.childImageSharp.fixed} /></button>
               <div className={reviewStyles.hide}>Hide Reviews</div>
             </div>
 
-            <div className={reviewStyles.noReviews}>Be the first to review this product!</div>
-          {/* <ReviewBox /> */}
-          <ReviewModal />
+            <div className="d-none">
+              <div className={reviewStyles.noReviews}>Be the first to review this product!</div>
+            </div>
+            <ReviewBox />
+            <ReviewModal />
+            <WelcomeModal />
           </div>
         </div>
       </div>
