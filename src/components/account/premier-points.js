6fc318d67c86b7046a6b8b35beff435ffbe8f8f1
Fatema <fgamal@effvision.com>
2020-10-01 21:10:43 +0200

    premier-points update

--- src/components/account/premier-points.js
+++ src/components/account/premier-points.js
@@ -52,9 +52,9 @@ export default function Premier() {
             <div className="col-12 col-lg-6 mob-p-0">
             <div className={[premierStyles.PremierRight,"show-mob"].join(" ")}>
                         <h1 className={premierStyles.Premiertitle}>
-                        Join Obagi Medical Premier Points Today
+                        Obagi Premier Points - Registered
                         </h1>
-                        <p className={premierStyles.Premiersubtitle}>Earn reward points with every purchase of Obagi Medical and receive free products*</p>
+                        <p className={premierStyles.Premiersubtitle}>Obagi Medical Register</p>
                      </div>
                 <Img className="mobileImg" fluid={data.bitmap.childImageSharp.fluid}/>
                 <div className={[premierStyles.centerimages].join(" ")}>
@@ -65,9 +65,9 @@ export default function Premier() {
             <div className="col-12 col-lg-6 mob-p-0">
                     <div className={premierStyles.PremierRight}>
                         <h1 className={[premierStyles.Premiertitle,"hide-mob"].join(" ")}>
-                        Join Obagi Medical Premier Points Today
+                         Obagi Premier Points - Registered
                         </h1>
-                        <p className={[premierStyles.Premiersubtitle,"hide-mob"].join(" ")}>Earn reward points with every purchase of Obagi Medical and receive free products*</p>
+                        <p className={[premierStyles.Premiersubtitle,"hide-mob"].join(" ")}>Obagi Medical Register</p>
                         <p className={premierStyles.PremierDesc}>Obagi Premier Points is an exclusive opportunity to earn points every time you purchase Obagi Medical products from your physician. Join today, and every dollar you spend in a physicians office goes towards free products in the future. Once you've saved enough points, you can redeem them for free Obagi products. Follow these 3 easy steps:</p>
                       <div className={premierStyles.PremierLinks}>
                     <div className={premierStyles.PremierLink}>
