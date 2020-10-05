31b689ee1950a11a3f6e6b7181241dc5af7a8486
Ahmed Bahieddine <abahi@effvision.com>
2020-10-04 20:50:21 -0400

    search updates

--- src/providers/search-provider.js
+++ src/providers/search-provider.js
@@ -2,8 +2,7 @@ import React, { createContext, useState } from 'react';
 
 const SearchContext = createContext();
 const baseUrl = process.env.Base_URL;
-
-
+const ProductsIndex= {};
 export const SearchProvider = ({children}) => {
 
     const [isLoading, setIsLoading] = useState(false);
@@ -11,34 +10,53 @@ export const SearchProvider = ({children}) => {
     const [clinicalSearchResults, setClinicalValue] = useState({});
     const [medicalSearchResults, setMedicalValue] =  useState({});
     const [searchWord, setsearchWord] =  useState({});
-  async  function  search(key){
+    const search = async (key) =>{
     const savekey = key;
-    let searchkey= key
-  if ( searchkey.length >2) {
+        let searchkey= key
+        if ( searchkey.length >2) {
+            
+            setIsLoading(true);
+            const getClinicalSearch =  await (await fetch(`${baseUrl}jsonapi/node/clinical_product?filter[title][condition][value]=${searchkey}&filter[title][condition][path]=title&filter[title][condition][operator]=CONTAINS`, {
+                method: 'GET',
+                credentials: 'include',
+                mode: 'cors',
+            
+            })).json();
+            setClinicalValue(getClinicalSearch);
+            const getMedicalSearch =  await (await fetch(`${baseUrl}jsonapi/node/medical_product?filter[title][condition][value]=${searchkey}&filter[title][condition][path]=title&filter[title][condition][operator]=CONTAINS`, {
+                method: 'GET',
+                credentials: 'include',
+                mode: 'cors',
+            
+            })).json();
+            setMedicalValue(getMedicalSearch); 
+            setsearchWord(savekey)
+
+            setIsLoading(false);
+        } 
+    }
+    const setSearchIndex = (ClinicalProduct,MedicalProduct) =>{
+        ProductsIndex.ClinicalProduct = ClinicalProduct;
+        ProductsIndex.MedicalProduct = MedicalProduct;
+        console.log("hafezz",ProductsIndex)
+    }
+    const searchInIndex = (searchkey) =>{
+        setIsLoading(true);
+        setsearchWord(searchkey);
+        let filterdClinicalProduct = ProductsIndex.ClinicalProduct.nodes.filter(function(itm){
+            return itm.title.toLowerCase().includes(searchkey) && searchkey != "";
+          });
+        let filterdMedicalProduct = ProductsIndex.MedicalProduct.nodes.filter(function(itm){
+        return itm.title.toLowerCase().includes(searchkey) && searchkey != "";
+        });
+        setClinicalValue(filterdClinicalProduct);
+        setMedicalValue(filterdMedicalProduct); 
+        console.log("hafezz",searchkey,filterdClinicalProduct,filterdMedicalProduct);
+        setIsLoading(false);
+    }
     
-  setIsLoading(true);
-  const getClinicalSearch =  await (await fetch(`${baseUrl}jsonapi/node/clinical_product?filter[title][condition][value]=${searchkey}&filter[title][condition][path]=title&filter[title][condition][operator]=CONTAINS`, {
-      method: 'GET',
-      credentials: 'include',
-      mode: 'cors',
-   
-  })).json();
-  setClinicalValue(getClinicalSearch);
-  const getMedicalSearch =  await (await fetch(`${baseUrl}jsonapi/node/medical_product?filter[title][condition][value]=${searchkey}&filter[title][condition][path]=title&filter[title][condition][operator]=CONTAINS`, {
-    method: 'GET',
-    credentials: 'include',
-    mode: 'cors',
- 
-})).json();
-setMedicalValue(getMedicalSearch); 
-setsearchWord(savekey)
-
-setIsLoading(false);
-} 
-   }
-
     return (
-        <SearchContext.Provider value={{search, isLoading , medicalSearchResults , clinicalSearchResults,searchWord}}>
+        <SearchContext.Provider value={{search,setSearchIndex,searchInIndex, isLoading , medicalSearchResults , clinicalSearchResults,searchWord}}>
             {children}
         </SearchContext.Provider>
     )
