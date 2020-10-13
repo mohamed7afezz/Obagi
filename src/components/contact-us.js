0f7264c9b8be898c484c8bb33fc51965152e1a6a
Ashraqat <ashraqatelshaabany@gmail.com>
2020-10-13 20:04:36 +0200

    updates

--- src/components/contact-us.js
+++ src/components/contact-us.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react"
 import { Link } from 'gatsby'
 import Customer from '../components/customer-care'
 import { CustomSelect } from '../assets/js/custom-select'
+import myAccountStyles from '../assets/scss/components/my-account.module.scss'
 
 export default function Contact() {
 
@@ -18,27 +19,45 @@ export default function Contact() {
             <div className="contact-us">
                 <div className="row">
                     <div className="col-12 col-lg-8">
+                        <div className="d-none d-lg-flex second-title-wrapper">
+                            <div className={myAccountStyles.secondTitle}>Contact Us</div>
+                        </div>
                         <div className="contact-text">If you have any questions or want more information about Obagi, please contact us. We love hearing from our customers and we would be glad to further assist you.</div>
                         <div className="contact-title">Email Obagi</div>
                     </div>
                 </div>
+
+
+                    {/* The below row -ONLY- appears after the submit request is successful  *****************/}
+
+                {/* <div className="row">
+                    <div className="col-12 col-lg-8">
+                        <div className="contact-thanks-note">Thank you for contacting Obagi</div>
+                        <div className="contact-thanks-text">We’ve received your message. You should receive a response within 2 to 3 business days.</div>
+                    </div>
+                </div> */}
+
                 <div className="row">
-                    <div className="col-12 col-lg-6">
+                    <div className="col-12 col-lg-8">
                         <div className="contact-text">Submit your message below, and we will respond to most inquiries within 2 to 3 business days.</div>
+                    </div>
+                    <div className="col-12 col-lg-6">
                         <form>
                             <div className="required-field">*All fields required</div>
 
 
                             <div className="check-group">
                                 <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="exampleRadios" id="contactFirstRadio" value="option1" />
-                                    <label className="form-check-label" for="contactFirstRadio">
+                                    <label className="radioLabel form-check-label" for="contactFirstRadio">
+                                        <input className="form-check-input" type="radio" name="exampleRadios" id="contactFirstRadio" value="option1" />
+                                        <span class="radiomark"></span>
                                         I am a Patient/Consumer
                                     </label>
                                 </div>
                                 <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="exampleRadios" id="contactSecondRadio" value="option2" />
-                                    <label className="form-check-label" for="contactSecondRadio">
+                                    <label className="radioLabel form-check-label" for="contactSecondRadio">
+                                        <input className="form-check-input" type="radio" name="exampleRadios" id="contactSecondRadio" value="option2" />
+                                        <span class="radiomark"></span>
                                         I am a Physician/Skin Care Professional
                                     </label>
                                 </div>
@@ -80,14 +99,16 @@ export default function Contact() {
 
                             <div className="second-check-group">
                                 <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="emailRadio" id="contactEmailRadio" value="option1" />
-                                    <label className="form-check-label" for="contactEmailRadio">
+                                    <label className="radioLabel form-check-label" for="contactEmailRadio">
+                                        <input className="form-check-input" type="radio" name="emailRadio" id="contactEmailRadio" value="option1" />
+                                        <span class="radiomark"></span>
                                         Email
                                 </label>
                                 </div>
                                 <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="phoneRadio" id="contactPhoneRadio" value="option2" />
-                                    <label className="form-check-label" for="contactPhoneRadio">
+                                    <label className="radioLabel form-check-label" for="contactPhoneRadio">
+                                        <input className="form-check-input" type="radio" name="phoneRadio" id="contactPhoneRadio" value="option2" />
+                                        <span class="radiomark"></span>
                                         Phone
                                 </label>
                                 </div>
