a99f188b253a5cf1559d94f318bfd7c31a53be0e
Ashraqat <ashraqatelshaabany@gmail.com>
2020-10-14 20:55:28 +0200

    skinanalyzer updates

--- src/components/skin-analyzer/q7.js
+++ src/components/skin-analyzer/q7.js
@@ -26,22 +26,74 @@ const Q7 = (props) => {
             <div>
                 {checkDataCondition((props.brandJourney=="Clinical Persona"),
                     <>
-                        <input type="radio" id="Retinol" name="q" value="Retinol" onChange={handleInputChange}/>
-                        <label htmlFor="Retinol">Retinol</label>
-                        <input type="radio" id="GlycolicAcid" name="q" value="Glycolic Acid" onChange={handleInputChange}/>
-                        <label htmlFor="GlycolicAcid">Glycolic Acid</label>
-                        <input type="radio" id="SalicylicAcid" name="q" value="Salicylic Acid" onChange={handleInputChange}/>
-                        <label htmlFor="SalicylicAcid">Salicylic Acid</label>
+                        <input type="radio" id="RetinolC" name="q" value="Retinol" onChange={handleInputChange}/>
+                        <label htmlFor="RetinolC">Retinol</label>
+
+                        <input type="radio" id="GlycolicAcidC" name="q" value="Glycolic Acid" onChange={handleInputChange}/>
+                        <label htmlFor="GlycolicAcidC">Glycolic Acid</label>
+
+                        <input type="radio" id="SalicylicAcidC" name="q" value="Salicylic Acid" onChange={handleInputChange}/>
+                        <label htmlFor="SalicylicAcidC">Salicylic Acid</label>
+
+                        <input type="radio" id="LacticAcidC" name="q" value="Lactic Acid" onChange={handleInputChange}/>
+                        <label htmlFor="LacticAcidC">Lactic Acid</label>
+                        
+                        <input type="radio" id="VitaminCC" name="q" value="Vitamin C" onChange={handleInputChange}/>
+                        <label htmlFor="VitaminCC">Vitamin C</label>
+
+                        <input type="radio" id="KinetinGrowthFactorsC" name="q" value="Kinetin (Growth Factors)" onChange={handleInputChange}/>
+                        <label htmlFor="KinetinGrowthFactorsC">Kinetin (Growth Factors)</label>
+
+                        <input type="radio" id="ArbutinC" name="q" value="Arbutin" onChange={handleInputChange}/>
+                        <label htmlFor="ArbutinC">Arbutin</label>
                     </>
                 )}
                 {checkDataCondition((props.brandJourney!="Clinical Persona"),
                     <>
                         <input type="radio" id="GlycolicAcid" name="q" value="Glycolic Acid" onChange={handleInputChange}/>
                         <label htmlFor="GlycolicAcid">Glycolic Acid</label>
+
                         <input type="radio" id="HyaluronicAcid" name="q" value="Hyaluronic Acid" onChange={handleInputChange}/>
                         <label htmlFor="HyaluronicAcid">Hyaluronic Acid</label>
+
                         <input type="radio" id="Niacinamide" name="q" value="Niacinamide" onChange={handleInputChange}/>
                         <label htmlFor="Niacinamide">Niacinamide</label>
+
+                        <input type="radio" id="Salicylicacid" name="q" value="Salicylic acid" onChange={handleInputChange}/>
+                        <label htmlFor="Salicylicacid">Salicylic acid</label>
+                        
+                        <input type="radio" id="Caffeine" name="q" value="Caffeine" onChange={handleInputChange}/>
+                        <label htmlFor="Caffeine">Caffeine</label>
+
+                        <input type="radio" id="Hydroquinone" name="q" value="Hydroquinone" onChange={handleInputChange}/>
+                        <label htmlFor="Hydroquinone">Hydroquinone</label>
+
+                        <input type="radio" id="Lacticacid" name="q" value="Lactic acid" onChange={handleInputChange}/>
+                        <label htmlFor="Lacticacid">Lactic acid</label>
+
+                        <input type="radio" id="BenzoylPeroxide" name="q" value="Benzoyl Peroxide" onChange={handleInputChange}/>
+                        <label htmlFor="BenzoylPeroxide">Benzoyl Peroxide</label>
+
+                        <input type="radio" id="Glycerin" name="q" value="Glycerin" onChange={handleInputChange}/>
+                        <label htmlFor="Glycerin">Glycerin</label>
+
+                        <input type="radio" id="Peptides" name="q" value="Peptides" onChange={handleInputChange}/>
+                        <label htmlFor="Peptides">Peptides</label>
+
+                        <input type="radio" id="Arbutin" name="q" value="Arbutin" onChange={handleInputChange}/>
+                        <label htmlFor="Arbutin">Arbutin</label>
+
+                        <input type="radio" id="AHAsBHAsPHAs" name="q" value="AHAs / BHAs / PHAs" onChange={handleInputChange}/>
+                        <label htmlFor="AHAsBHAsPHAs">AHAs / BHAs / PHAs</label>
+
+                        <input type="radio" id="VitaminC" name="q" value="Vitamin-C" onChange={handleInputChange}/>
+                        <label htmlFor="VitaminC">Vitamin-C</label>
+
+                        <input type="radio" id="Retinol" name="q" value="Retinol" onChange={handleInputChange}/>
+                        <label htmlFor="Retinol">Retinol</label>
+
+                        <input type="radio" id="Antioxidants" name="q" value="Antioxidants" onChange={handleInputChange}/>
+                        <label htmlFor="Antioxidants">Antioxidants</label>
                     </>
                 )}
                 <button onClick={sendBackData}>See Your Results</button>
