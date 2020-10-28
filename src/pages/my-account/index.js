3d413c6a740ef685a903c916af7f04fc23ad2f6f
Mohamed Hafez - devteam2 <mhafez@effvision.com>
2020-10-28 19:49:13 +0200

    updates

--- src/pages/my-account/index.js
+++ src/pages/my-account/index.js
@@ -16,7 +16,7 @@ export default function MyAccount() {
     return (
         <Layout>
             <Router>
-                {/* <PrivateRoute path="/my-account" component={UserAccount} /> */}
+                <PrivateRoute path="/my-account" component={UserAccount} />
                 <PrivateRoute path="/my-account/show-account" component={ShowAccount}/>
                 <PrivateRoute path="/my-account/orders" component={Orders} />
                 <PrivateRoute path="/my-account/address-book" component={AddressBook} />
