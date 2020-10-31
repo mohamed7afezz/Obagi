514f7829e63468fe9fe13eadcd1c448dd7a47168
Ahmed Bahieddine <abahi@effvision.com>
2020-10-31 14:00:38 -0400

    edits

--- src/components/footer.js
+++ src/components/footer.js
@@ -273,7 +273,7 @@ const Footer = ({ siteTitle }) => {
               <ul className="footerprivacy">
                 <Link to='/privacy-policy' className="footerprivacyli">Privacy Policy</Link>
                 <Link to='/terms-of-use' className="footerprivacyli">Terms of Use</Link>
-                <Link to='/cookie-policy' className="footerprivacyli">Cookie Policy</Link>
+                {/* <Link to='/cookie-policy' className="footerprivacyli">Cookie Policy</Link> */}
                 {/* <Link to='#' className="footerprivacyli">User Generated Content Terms</Link> */}
                 <Link to='/sitemap' className="footerprivacyli">Sitemap</Link>
                 <p id="demo"></p>
