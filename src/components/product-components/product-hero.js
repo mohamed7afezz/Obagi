b9204fb1d5b9b79eba70a580acd83195cdbd637e
A.Bahi <abahieddine@greaterthanone.com>
2020-11-19 13:20:49 -0500

    fixes

--- src/components/product-components/product-hero.js
+++ src/components/product-components/product-hero.js
@@ -420,7 +420,7 @@ const ProductHero = ({ data, nodeType }) => {
                 </select>
               </div>
 
-              <div className={["d-flex", ProductStyles.centeralign, "centeralign", "col-12 col-md-6", "col-lg-8"].join(" ")}>
+              <div className={["d-flex", ProductStyles.centeralign, "centeralign", "col-12 col-md-6", "col-lg-6"].join(" ")}>
                 {field_medical_rx == "RX" ?
                   <Link
                     className={["btn", ProductStyles.btnCart, "btnCart"].join(" ")}
