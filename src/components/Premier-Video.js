cb6d928c8b3ac9344deb25fb98202bb62e4bb7f4
Mohamed Hafez - devteam2 <mhafez@effvision.com>
2020-11-25 18:53:58 +0200

    premier video fix

--- src/components/Premier-Video.js
+++ src/components/Premier-Video.js
@@ -5,52 +5,54 @@ import playbtnimg from "../assets/images/product-images/PlayVideo.svg"
 import Player from '@vimeo/player';
 
 const PremierVideo = ({ node }) => {
-    function playvideo(event) {
-        event.preventDefault()
-        let iframeContainer, player, playerOpts = {
-            url: ''
-        }
-        
-        let url = event.target.parentNode.getAttribute("data-link");
+  function playvideo(event) {
+    event.preventDefault()
+    let iframeContainer, player, playerOpts = {
+      url: ''
+    }
 
-        playerOpts.url = url;
-       
-        if (!playerOpts.url.indexOf('youtube') > -1 || !playerOpts.url.indexOf('youtu') > -1) {
-            document.querySelector('.video-popup-wrap').innerHTML = '<iframe class="embed-responsive-item" src="' + url + '?rel=0&autoplay=true" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-            
-            return;
-        }
-    
-        player = new Player.Vimeo(document.querySelector('#VideoPopUp iframe'), playerOpts);
-    
-        player.play();
-    
+    let url = event.target.parentNode.getAttribute("data-link");
+
+    playerOpts.url = url;
+
+    if (!playerOpts.url.indexOf('youtube') > -1 || !playerOpts.url.indexOf('youtu') > -1) {
+      document.querySelector('.video-popup-wrap').innerHTML = '<iframe class="embed-responsive-item" src="' + url + '?rel=0&autoplay=true" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+      return;
     }
-    function SetVideoTime(time, video, endTime) {
-        video.setCurrentTime(time);
-        if (endTime) {
-            video.on('timeupdate', function (data) {
-                if (data.seconds > endTime) {
-                    video.pause();
-                    video.off('timeupdate');
-                }
-            })
+
+    player = new Player.Vimeo(document.querySelector('#VideoPopUp iframe'), playerOpts);
+
+    player.play();
+
+  }
+  function SetVideoTime(time, video, endTime) {
+    video.setCurrentTime(time);
+    if (endTime) {
+      video.on('timeupdate', function (data) {
+        if (data.seconds > endTime) {
+          video.pause();
+          video.off('timeupdate');
         }
+      })
     }
+  }
   return (
     <div className="permier-video-link video-link">
-      <Img className="video-img-mob" fluid={node.relationships.field_video_poster.localFile.childImageSharp.fluid}/>
-          <div className="video-wrapper">
-                {
-                    <div className="img-wrap">
-                        <a data-toggle="modal" data-target="#VideoPopUp" onClick={(e) => { playvideo(e) }} href="#" data-link={node.field_video_link} class=" popupvideo playbtn">
-                            <img class="playbtnimg mob-change" src={playbtnimg} alt="videomsg" />
-                        </a>
-                        <p className="text-wrap">{node.field_video_txt}</p>
-                    </div>
-                   
-                }
-            </div>
+      {node.relationships && node.relationships.field_video_poster && node.relationships.field_video_poster.localFile && node.relationships.field_video_poster.localFile.childImageSharp ?
+        <Img className="video-img-mob" fluid={node.relationships.field_video_poster.localFile.childImageSharp.fluid} />
+        : ""}
+      <div className="video-wrapper">
+        {
+          <div className="img-wrap">
+            <a data-toggle="modal" data-target="#VideoPopUp" onClick={(e) => { playvideo(e) }} href="#" data-link={node.field_video_link} class=" popupvideo playbtn">
+              <img class="playbtnimg mob-change" src={playbtnimg} alt="videomsg" />
+            </a>
+            {node.field_video_txt ? <p className="text-wrap">{node.field_video_txt}</p> : ""}
+          </div>
+
+        }
+      </div>
     </div>
   )
 }
