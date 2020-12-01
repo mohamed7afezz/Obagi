fd1bb8716f6b06d265bc0aa22dc94434e5c8c545
Ahmed Bahieddine <abahi@effvision.com>
2020-12-01 17:02:21 -0500

    revert-rx

--- src/components/collection-components/collectoin-products-list.js
+++ src/components/collection-components/collectoin-products-list.js
@@ -249,7 +249,7 @@ const Collectionproducts = ({ node, nodetype,checktaxonomyType }) => {
   }, [])
   function handlePrescribe(e) {
     if(e.target.checked) {
-      isoFilter('rx', ':not(.Non-RX)');
+      isoFilter('rx', ':not(.RX)');
     } else {
       isoFilter('rx', 'All');
     }
