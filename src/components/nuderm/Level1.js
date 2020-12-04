0f53144bd271b4ac4fc47dc773e967b5375a4fcf
Mohamed Hafez <mhafez@effvision.com>
2020-12-04 23:55:31 +0000

    nuderm years change

--- src/components/nuderm/Level1.js
+++ src/components/nuderm/Level1.js
@@ -294,11 +294,12 @@ function topFunction() {
                       <label for="yearCustom" className={["form-label", nudermStyle.customlabel].join(" ")}>YEAR</label>
                       <select name="year" className="form-control" id="yearCustom">
                       <option value="">Year</option>
-                      <option value="2018">2018</option>
-                      <option value="2019">2019</option>
                       <option value="2020">2020</option>
                       <option value="2021">2021</option>
                       <option value="2022">2022</option>
+                      <option value="2023">2023</option>
+                      <option value="2024">2024</option>
+                      <option value="2025">2025</option>
                       </select>
                     </div>
                   </div>
