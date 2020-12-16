31836c1345e11df953053e873d55e6fb133d91d9
Mohamed Hafez - devteam2 <mhafez@effvision.com>
2020-12-16 18:03:50 +0200

    home hero edits

--- src/components/home-hero.js
+++ src/components/home-hero.js
@@ -70,20 +70,12 @@ const HomeHero = ({ node }) => {
     slidesToShow: 1,
     arrows: false,
     dots: true,
-    // autoplay: true,
-    // autoplaySpeed: 4000,
+    autoplay: true,
+    autoplaySpeed: 4000,
     // beforeChange: (current, next) => {
     //     document.querySelector('.myslickactive').classList.remove('myslickactive');
     //     document.querySelectorAll("#product-hero-slick .imageContainer")[next].classList.add('myslickactive')
     // },
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          dots: false,
-        }
-      },
-    ]
   }
 
   console.log("ashh", node.relationships.field_box[1].relationships.field_background.localFile.childImageSharp.original.src)
