b38de6b82a9217236d4e734044acfa941fba8749
Ashraqat Mohamed <amohamed@effvision.com>
2021-06-03 15:22:27 -0400

    added min-quantity

--- src/components/skin-analyzer/resulte.js
+++ src/components/skin-analyzer/resulte.js
@@ -219,6 +219,7 @@ const Resulte = (props) => {
                                                 price={data.field_clinical_price}
                                                 productId={data.field_clinical_id}
                                                 Sku={data.field_clinical_sku}
+                                                minQuantity={data.field_min_quantity? data.field_min_quantity : ""}
                                             />
                                         }) : ''}
                                         <div className={[resulteSkinStyle.addtobagcon, resulteSkinStyle.addtobagcondata].join(" ")}>
@@ -349,6 +350,7 @@ const Resulte = (props) => {
                                                 productId={data.field_medical_id}
                                                 Type={MedicalResultType[index]}
                                                 Sku={data.field_medical_sku ? data.field_medical_sku : ""}
+                                                minQuantity={data.field_min_quantity? data.field_min_quantity : ""}
                                                 premierid={data.field_medical_premier_points_id ? data.field_medical_premier_points_id : ""}
                                                 feild_preimer={data.field_medical_premier_points ? data.field_medical_premier_points : ""}
                                             />
