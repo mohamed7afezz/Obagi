2b0d0154ec785676f1ab38a0cd699f60c46010a0
Ashraqat Mohamed <amohamed@effvision.com>
2021-06-08 11:23:35 -0400

    first and second paragraphs in blog LP

--- src/components/hero-slider.js
+++ src/components/hero-slider.js
@@ -12,12 +12,12 @@ const HeroSlider = ({ node }) => {
     function scrollToSection(e, section) {
         e.preventDefault();
         let sectionId = section.split('#')[1];
-        
+
         window.scroll({ top: $(`#${sectionId}`).offset().top - $('#mob-navigation').height(), behavior: 'smooth' });
 
     }
     // useEffect(() => {
-       
+
     //         if ($(".scrollto").length) {
     //                // Add smooth scrolling to all links
     //             $(".scrollto").on('click', function(event) {
@@ -26,16 +26,16 @@ const HeroSlider = ({ node }) => {
     //             if (this.hash !== "") {
     //               // Prevent default anchor click behavior
     //               event.preventDefault();
-            
+
     //               // Store hash
     //               var hash = this.hash;
-            
+
     //               // Using jQuery's animate() method to add smooth page scroll
     //               // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
     //               $('html, body').animate({
     //                 scrollTop: $(hash).offset().top
     //               }, 500, function(){
-            
+
     //                 // Add hash (#) to URL when done scrolling (default click behavior)
     //                 window.location.hash = hash;
     //               });
@@ -45,7 +45,7 @@ const HeroSlider = ({ node }) => {
     // })
 
     // useEffect(() => {
-     
+
     //     let scrollSection = document.querySelector("#here");
     //     let scrollButton = document.querySelector("#slideDownButton");
     //     if (typeof window !== "undefined" ) {
@@ -54,12 +54,12 @@ const HeroSlider = ({ node }) => {
     //                 scrollButton.classList.remove("d-none");
     //                 scrollButton.classList.add("upsideButton");
 
-    
+
     //             } else {
     //                 scrollButton.classList.add("d-none");
     //                 scrollButton.classList.remove("upsideButton");
     //             }
-    
+
     //         })
     //     }
 
@@ -87,8 +87,8 @@ const HeroSlider = ({ node }) => {
         ]
     }
 
-    let pageType = node.field_page_type? node.field_page_type : null
-    let heroClass = node.field_slider_class? node.field_slider_class : null
+    let pageType = node.field_page_type ? node.field_page_type : null
+    let heroClass = node.field_slider_class ? node.field_slider_class : null
 
     function fixlink(changelink) {
 
@@ -113,8 +113,8 @@ const HeroSlider = ({ node }) => {
 
 
 
-        <div className={pageType && pageType === 'medical' ? "container-fluid pl-0 pr-0 hero-slider medical-slider " : pageType && pageType === 'clinical' ? 'container-fluid pl-0 pr-0 hero-slider clinical-slider ' : 'container-fluid pl-0 pr-0 hero-slider '  + `${heroClass? heroClass : "" }`}>
-            <div className={pageType ? (pageType === 'clinical' ? "row mr-0 ml-0 " + heroSlider.rowWrapper + " " + heroSlider.textWrapperClinical : pageType === 'medical' ? "row mr-0 ml-0 " + heroSlider.rowWrapper + " " + heroSlider.textWrapperMedical : '') : ''}>
+        <div className={pageType && pageType === 'medical' ? "container-fluid pl-0 pr-0 hero-slider medical-slider " : pageType && pageType === 'clinical' ? 'container-fluid pl-0 pr-0 hero-slider clinical-slider ' : 'container-fluid pl-0 pr-0 hero-slider ' + `${heroClass ? heroClass : ""}`}>
+            <div className={pageType ? (pageType === 'clinical' ? "row mr-0 ml-0 " + heroSlider.rowWrapper + " " + heroSlider.textWrapperClinical : pageType === 'medical' ? "row mr-0 ml-0 " + heroSlider.rowWrapper + " " + heroSlider.textWrapperMedical : '') : "row mr-0 ml-0 " + heroSlider.rowWrapper}>
                 <div style={{ width: "100%" }}>
                     <Slider {...SliderSetting}>
                         {node.relationships.field_slide.map((item, index) => {
@@ -124,18 +124,18 @@ const HeroSlider = ({ node }) => {
                                         <div className="col-12 col-lg-7 col-padding">
                                             {item.relationships.field_slide_image ? item.relationships.field_slide_image.localFile ? <div><img alt="img" className={heroSlider.mobImage} src={item.relationships.field_slide_image.localFile.childImageSharp.original.src} /></div> : '' : ""}
                                         </div>
-                                        <div className={pageType ? (pageType === 'clinical' ? heroSlider.textWrapperClinical : pageType === 'medical' ? heroSlider.textWrapperMedical : '') : ''}>
+                                        <div className={pageType ? (pageType === 'clinical' ? heroSlider.textWrapperClinical : pageType === 'medical' ? heroSlider.textWrapperMedical : '') : 'textWrapperSlider'}>
                                             <div className="col-12 col-lg-4">
                                                 {item.field_slide_type ? <div dangerouslySetInnerHTML={{ __html: item.field_slide_type.processed }} className={["subtitle", heroSlider.subtitle, pageType && pageType.includes('medical') ? heroSlider.medical : ''].join(" ")}></div> : ''}
                                                 {item.field_slide_title ? <div dangerouslySetInnerHTML={{ __html: item.field_slide_title.processed }} className={[heroSlider.title, "heroTitle"].join(" ")}></div> : ''}
                                                 {item.field_sli ? <div dangerouslySetInnerHTML={{ __html: item.field_sli.processed }} className={[heroSlider.description, "heroDesc"].join(" ")}></div> : ''}
                                                 {item.field_slide_title_sample ? <div dangerouslySetInnerHTML={{ __html: item.field_slide_title_sample.processed }} className={[heroSlider.description, "heroDesc"].join(" ")}></div> : ''}
-                                                {item.field_slide_button ? <div className={heroSlider.linkSection}>
-                                                    {item.field_slide_button.uri.replace('internal:', '').includes('#')? <a onClick={(e) => {scrollToSection(e, item.field_slide_button.uri.replace('internal:', ''))}} className={["button-link", `${heroClass === 'vitaminc'? 'scrollto' : ''}`].join(" ")} href={'#entertowin'}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></a> : 
-                                                        <Link to={item.field_slide_button.uri.replace('internal:', '')} className={["button-link", `${heroClass === 'vitaminc'? 'scrollto' : ''}`].join(" ")}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></Link>
+                                                {item.field_slide_button ? <div className={`heroLink ${heroSlider.linkSection}`}>
+                                                    {item.field_slide_button.uri.replace('internal:', '').includes('#') ? <a onClick={(e) => { scrollToSection(e, item.field_slide_button.uri.replace('internal:', '')) }} className={["button-link", `${heroClass === 'vitaminc' ? 'scrollto' : ''}`].join(" ")} href={'#entertowin'}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></a> :
+                                                        <Link to={item.field_slide_button.uri.replace('internal:', '')} className={["button-link", `${heroClass === 'vitaminc' ? 'scrollto' : ''}`].join(" ")}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></Link>
                                                     }
-                                                    
-                                                    </div> : ''}
+
+                                                </div> : ''}
                                             </div>
                                         </div>
                                     </div>
@@ -150,10 +150,10 @@ const HeroSlider = ({ node }) => {
                                                     {item.field_sli ? <div dangerouslySetInnerHTML={{ __html: item.field_sli.processed }} className={[heroSlider.description, "heroDesc"].join(" ")}></div> : ''}
                                                     {item.field_slide_title_sample ? <div dangerouslySetInnerHTML={{ __html: item.field_slide_title_sample.processed }} className={[heroSlider.description, "heroDesc"].join(" ")}></div> : ''}
                                                     {item.field_slide_button ? <div className={[heroSlider.linkSection, "heroLink"].join(" ")}>
-                                                        
-                                                        {item.field_slide_button.uri.replace('internal:', '').includes('#')? <a onClick={(e) => {scrollToSection(e, item.field_slide_button.uri.replace('internal:', ''))}} className={["button-link", heroSlider.link, `${heroClass === 'vitaminc'? 'scrollto' : ''}`].join(" ")} href={'#entertowin'}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></a> : 
-                                                        <Link to={item.field_slide_button.uri.replace('internal:', '')} className={["button-link", heroSlider.link, `${heroClass === 'vitaminc'? 'scrollto' : ''}`].join(" ")}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></Link>
-                                                    }
+
+                                                        {item.field_slide_button.uri.replace('internal:', '').includes('#') ? <a onClick={(e) => { scrollToSection(e, item.field_slide_button.uri.replace('internal:', '')) }} className={["button-link", heroSlider.link, `${heroClass === 'vitaminc' ? 'scrollto' : ''}`].join(" ")} href={'#entertowin'}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></a> :
+                                                            <Link to={item.field_slide_button.uri.replace('internal:', '')} className={["button-link", heroSlider.link, `${heroClass === 'vitaminc' ? 'scrollto' : ''}`].join(" ")}><span dangerouslySetInnerHTML={{ __html: item.field_slide_button.title }}></span></Link>
+                                                        }
                                                     </div> : ''}
                                                 </div>
                                             </div>
@@ -175,12 +175,14 @@ const HeroSlider = ({ node }) => {
                     </Slider>
                 </div>
 
-                {node.relationships.field_obagi_logo && node.relationships.field_obagi_logo.localFile ?
-                    <div className={["d-none d-lg-block col-lg-5", heroSlider.blueSection].join(" ")}>
-                        {/* {node.relationships.field_slider_scroll_down ? (node.relationships.field_slider_scroll_down.localFile ? <button id="slideDownButton" className="scroll-button d-none" onClick={(e) => {scrollUp(e);}}><Img alt="img"  fixed={node.relationships.field_slider_scroll_down.localFile.childImageSharp.fixed} /></button> : '') : ''} */}
-                        <div className={heroSlider.sliderLogo}><Img alt="img"  fixed={node.relationships.field_obagi_logo.localFile.childImageSharp.fixed} /></div> 
-                    </div>
-                : ''}
+
+                <div className={["d-none d-lg-block col-lg-5 heroSliderBlue", heroSlider.blueSection].join(" ")}>
+                    {/* {node.relationships.field_slider_scroll_down ? (node.relationships.field_slider_scroll_down.localFile ? <button id="slideDownButton" className="scroll-button d-none" onClick={(e) => {scrollUp(e);}}><Img alt="img"  fixed={node.relationships.field_slider_scroll_down.localFile.childImageSharp.fixed} /></button> : '') : ''} */}
+                    {node.relationships.field_obagi_logo && node.relationships.field_obagi_logo.localFile ?
+                        <div className={heroSlider.sliderLogo}><Img alt="img" fixed={node.relationships.field_obagi_logo.localFile.childImageSharp.fixed} /></div>
+                        : ''}
+                </div>
+
                 {/* <div id="here" className={heroSlider.here}></div> */}
 
             </div>
--- src/components/section-with-colored-boxes.js
+++ src/components/section-with-colored-boxes.js
@@ -7,19 +7,20 @@ import coloredBoxesStyle from '../assets/scss/components/section-with-colored-bo
 
 const SectionWithColoredBoxes = ({ node }) => {
 
+  let blogPage = node.field_colored_custom_class && node.field_colored_custom_class == "blog-lp"? true : false;
 
   return (
     <div className={(node.field_not_homepage && node.field_image_is_right)?`${node.field_colored_custom_class? node.field_colored_custom_class : ""}` + " " + coloredBoxesStyle.notHome + " container-fluid " + coloredBoxesStyle.imageRight : (!node.field_image_is_right && node.field_not_homepage)? `${node.field_colored_custom_class? node.field_colored_custom_class : ""}` + " container-fluid " + coloredBoxesStyle.notHome : "container-fluid " + `${node.field_colored_custom_class? node.field_colored_custom_class : ""}`} id="colored-boxes">
       <div className={["row colored-big-section", coloredBoxesStyle.bigSection].join(" ")}>
-        <div className={!node.field_not_homepage && node.field_colored_custom_class? "col-10 offset-2 col-lg-5 offset-lg-0 colored-first-section "+ coloredBoxesStyle.colLeftPadding + " " + coloredBoxesStyle.firstSection : !node.field_not_homepage ? "col-10 col-lg-5 "+ coloredBoxesStyle.colLeftPadding + " " + coloredBoxesStyle.firstSection : "col-12  col-lg-5 "+ coloredBoxesStyle.colLeftPadding + " " + coloredBoxesStyle.colRightPadding + " " + coloredBoxesStyle.firstSection}>
+        <div className={blogPage? `col-12 col-lg-5 colored-first-section ${coloredBoxesStyle.colLeftPadding} ${coloredBoxesStyle.firstSection}`  : !node.field_not_homepage && node.field_colored_custom_class? "col-10 offset-2 col-lg-5 offset-lg-0 colored-first-section "+ coloredBoxesStyle.colLeftPadding + " " + coloredBoxesStyle.firstSection : !node.field_not_homepage ? "col-10 col-lg-5 "+ coloredBoxesStyle.colLeftPadding + " " + coloredBoxesStyle.firstSection : "col-12  col-lg-5 "+ coloredBoxesStyle.colLeftPadding + " " + coloredBoxesStyle.colRightPadding + " " + coloredBoxesStyle.firstSection}>
           {node.relationships && node.relationships.field_colored_boxes_image && node.relationships.field_colored_boxes_image.localFile && node.relationships.field_colored_boxes_image.localFile.childImageSharp? <div className={[coloredBoxesStyle.image, "colored-image"].join(" ")}><Img alt="img"  fluid={node.relationships.field_colored_boxes_image.localFile.childImageSharp.fluid} /></div> : ''}
         </div>
-        <div className={!node.field_not_homepage && node.field_colored_custom_class? "col-12 col-lg-7 colored-col-right " + coloredBoxesStyle.colRightPadding : !node.field_not_homepage? "col-9 offset-2 col-lg-7 offset-lg-0 " + coloredBoxesStyle.colRightPadding : (node.field_image_is_right && node.field_not_homepage)?  "col-12 col-lg-6 offset-lg-1" : "col-12 col-lg-6 offset-lg-0"}>
+        <div className={blogPage? `col-12 col-lg-6 colored-col-right ${coloredBoxesStyle.colRightPadding}` : !node.field_not_homepage && node.field_colored_custom_class? "col-12 col-lg-7 colored-col-right " + coloredBoxesStyle.colRightPadding : !node.field_not_homepage? "col-9 offset-2 col-lg-7 offset-lg-0 " + coloredBoxesStyle.colRightPadding : (node.field_image_is_right && node.field_not_homepage)?  "col-12 col-lg-6 offset-lg-1" : "col-12 col-lg-6 offset-lg-0"}>
           <div className={[coloredBoxesStyle.smallSection, "colored-small-section"].join(" ")}>
             {node.field_colored_boxes_subtitle? <div dangerouslySetInnerHTML={{ __html: node.field_colored_boxes_subtitle.processed }} className={[coloredBoxesStyle.subtitle, "colored-subtitle"].join(" ")}></div> : ""}
             {node.field_colored_boxes_title? <h3 dangerouslySetInnerHTML={{ __html: node.field_colored_boxes_title.processed }} className={[coloredBoxesStyle.title, "title colored-title"].join(" ")}></h3> : ""}
             {node.field_colored_boxes_description? <div dangerouslySetInnerHTML={{ __html: node.field_colored_boxes_description.processed }} className={[coloredBoxesStyle.description, "description colored-description"].join(" ")}></div> : ""}
-            {node.field_colored_boxes_button? <div className={[coloredBoxesStyle.linkSection].join(" ")}><Link to={node.field_colored_boxes_button.uri.replace('internal:', '')} className={[coloredBoxesStyle.link].join(" ")}>{node.field_colored_boxes_button.title}</Link></div> : ""}
+            {node.field_colored_boxes_button? <div className={[coloredBoxesStyle.linkSection, "colored-link"].join(" ")}><Link to={node.field_colored_boxes_button.uri.replace('internal:', '')} className={[coloredBoxesStyle.link].join(" ")}>{node.field_colored_boxes_button.title}</Link></div> : ""}
           </div>
         </div>
       </div>
